baa12a420c03e0419f926acde7b118cb870b94ba
Nicole Quinstedt <90027419+Quinstedt@users.noreply.github.com>
2025-01-30 16:40:43 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,70 +1,51 @@
 import argparse
 import os
-import 
+from lib import Session, Model, NegativeTokenCountError, InsufficientAllowedTokensError, TokenError
+from dotenv import load_dotenv
 
 load_dotenv()
 
 #############################################
 # Set up arguments
 #############################################
-parser = argparse.ArgumentParser(description='Process some file paths.')
-parser.add_argument(
-    '-p','--paths',
-    dest='paths', 
-    type=str, 
-    nargs='+', 
-    help='at least one file path',
-    required=True
-)
+parser = argparse.ArgumentParser(description='Model parameters.')
 
-parser.add_argument("-m", "--model", dest="model", type=str, default= "mistral_debugger",
+parser.add_argument("-m", "--model", dest="model", type=str, default="mistral_debugger",
                     help="Set the model to use")
-parser.add_argument('-t', '--temperature', dest="temperature", type=str, default=0.7,
-                    help='Path to the module file')
+parser.add_argument('-t', '--temperature', dest="temperature", type=float, default=0.7,
+                    help='Set the temperature value')
 
 args = parser.parse_args()
 model: str = args.model.lower()
-temperature = args.temperature #Default 0.7
+temperature = args.temperature  # Default 0.7
 
 #############################################
 # Load model
 #############################################
 
-from .util.model_lib import Session
-
 # Define a default path
-default_model = "" 
+default_model = "/mimer/NOBACKUP/groups/naiss2024-22-453/.cache/models--mistralai--Mistral-Small-Instruct-2409/snapshots/a5b29a334a0991ea7bb2d78ef9c7ca14ac9f8f04" 
 
- max_new_tokens = 2048
-
-if model == "mixtral":
-        model_path = os.getenv("MIXTRAL")
+if model == "mistral":
+    model_path = os.getenv("MISTRAL")
 elif model == "llama":
-        model_path = os.getenv("LLAMA")
+    model_path = os.getenv("LLAMA")
 else: 
     model_path = default_model
 
-system_prompt: str = """You are a helpful AI assistant"""
 
-myModel: Model = Model.get(
-    model_name_or_path = model,
-    max_new_tokens,
-    system_prompt,
-    temperature
+model: Session = Session.create(
+    name="Example Session",
+    model_name_or_path=model_path,
+    max_new_tokens=2048,  # required
+    temperature=temperature
 )
+
+
 #############################################
 # Prompt the model
 #############################################
 
-###########
-# The library only provide the user prompt and response, if we want to also keep track of the used system prompt, 
-# you will need to add it to the chatHistory
-#
-# chatHistory = [{"role": "system", "content": self.system_prompt}]
-##############
-
-prompt = "Tell me a joke"
-chatHistory = []
-response = model.get_response([], prompt)
-print("Model response" , response)
-
+user_prompt = "Tell me a joke"
+response = model.prompt(user_prompt) 
+print("Model response", response)
